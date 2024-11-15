8d6ecb29a74f966bc49e37ef9956c0568912871f
谢彬彬 <xiebinbin@sodagreen.love>
2024-11-15 14:15:05 +0800

    兼容

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,9 @@
 use encoding_rs::GBK;
 use std::path::PathBuf;
+#[cfg(unix)]
 use std::{env, os::unix::ffi::OsStrExt};
+#[cfg(windows)]
+use std::{env, os::windows::ffi::OsStrExt};
 use tauri::command;
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
@@ -12,17 +15,21 @@ fn greet(name: &str) -> String {
 fn get_current_executable_name() -> Option<String> {
     fn decode_path_name(path: PathBuf) -> Option<String> {
         let name = path.file_name()?;
+        
+        #[cfg(unix)]
         let bytes = name.as_bytes();
+        #[cfg(not(windows))]
+        let bytes = name.to_string_lossy().as_bytes().to_vec();
 
         // 尝试 UTF-8 解码
-        if let Ok(name_str) = std::str::from_utf8(bytes) {
+        if let Ok(name_str) = std::str::from_utf8(&bytes) {
             // name_str 已经是 UTF-8 编码
             return Some(name_str.to_owned());
         }
 
         // 如果 UTF-8 解码失败，尝试用 GBK 解码
         // GBK.decode() 会返回 UTF-8 编码的 Cow<str>
-        let (cow, _encoding_used, had_errors) = GBK.decode(bytes);
+        let (cow, _encoding_used, had_errors) = GBK.decode(&bytes);
         if !had_errors {
             // cow 已经是 UTF-8 编码
             return Some(cow.into_owned());
